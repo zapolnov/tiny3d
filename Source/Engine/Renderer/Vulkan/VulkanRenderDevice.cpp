a904a44430bc4a5e9c94b1f7555e989fa9fe2a1b
Nikolay Zapolnov <zapolnov@gmail.com>
2020-03-03 17:46:02 +0200

    Textures fix.

--- Source/Engine/Renderer/Vulkan/VulkanRenderDevice.cpp
+++ Source/Engine/Renderer/Vulkan/VulkanRenderDevice.cpp
@@ -738,7 +738,7 @@ std::unique_ptr<ITexture> VulkanRenderDevice::createTexture(const TextureData* d
     viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
     viewInfo.image = texture;
     viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-    viewInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+    viewInfo.format = VK_FORMAT_R8G8B8A8_UNORM;
     viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
     viewInfo.subresourceRange.baseMipLevel = 0;
     viewInfo.subresourceRange.levelCount = 1;
